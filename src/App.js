cf4f650ebed0c7c6c58cfa9e4b5a1c7215e264b0
Yun Yul <dbsdbf95@gmail.com>
2025-05-12 12:39:01 +0900

    router 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SplashPage from './pages/SplashPage';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
